1a177ee53a96759ddf614a00a8cd3228ab9acad5
Max Alva <alva85@gmail.com>
2021-02-11 09:05:48 -0500

    Protección de rutas

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
+import React, {useContext} from 'react';
+import {useHistory} from 'react-router-dom';
+import {AuthContext} from "../../auth/AuthContext";
+import {types} from "../../types/types";
 
 const LoginScreen = () => {
 
-	const history = useHistory();
+    const history = useHistory();
 
-	const handleLogin = () => {
-		history.replace( '/' );
-	};
+    const lastPath = localStorage.getItem('lastPath') || '/';
 
-	return (
-		<div className="container mt-5">
-			<h1>Login</h1>
-			<hr/>
+    const {dispatch} = useContext(AuthContext);
 
-			<button className="btn btn-primary" onClick={handleLogin}>
-				Login
-			</button>
-		</div>
-	);
+    const handleLogin = () => {
+        dispatch({
+            type: types.login,
+            payload: {
+                name: 'Max'
+            }
+        });
+        history.replace(lastPath);
+    };
+
+    return (
+        <div className="container mt-5">
+            <h1>Login</h1>
+            <hr/>
+
+            <button className="btn btn-primary" onClick={handleLogin}>
+                Login
+            </button>
+        </div>
+    );
 };
 
 export default LoginScreen;
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {
-	BrowserRouter as Router,
-	Switch,
-	Route
+    BrowserRouter as Router,
+    Switch
 } from 'react-router-dom';
 import LoginScreen from '../components/login/LoginScreen';
 import DashboardRoutes from './DashboardRoutes';
+import PrivateRoute from "./PrivateRoute";
+import {AuthContext} from "../auth/AuthContext";
+import PublicRoute from "./PublicRoute";
 
 const AppRouter = () => {
-	return (
-		<Router>
-			<div>
-				<Switch>
-					<Route exact path="/login" component={LoginScreen}/>
 
-					<Route path="/" component={DashboardRoutes}/>
-				</Switch>
-			</div>
-		</Router>
-	);
+    const {user: {logged}} = useContext(AuthContext)
+
+    return (
+        <Router>
+            <div>
+                <Switch>
+                    <PublicRoute
+                        exact
+                        path="/login"
+                        isAuthenticated={logged}
+                        component={LoginScreen}/>
+
+
+                    <PrivateRoute
+                        path="/"
+                        isAuthenticated={logged}
+                        component={DashboardRoutes}/>
+                </Switch>
+            </div>
+        </Router>
+    );
 };
 
 export default AppRouter;
