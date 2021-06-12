472c517f38cf546dcdf842e69653407943278215
Max Alva <alva85@gmail.com>
2021-06-12 13:49:06 -0500

    tests

---
+++ src/test/routers/PrivateRoute.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { mount } from 'enzyme';
+import PrivateRoute from '../../routers/PrivateRoute';
+
+describe( 'Pruebas en <PrivateRoute />', function () {
+
+    const props = {
+        location: {
+            pathname: '/marvel'
+        }
+    };
+
+    test( 'debe mostrar el componente si está autenticado y guardar localStorage', () => {
+
+        const wrapper = mount(
+            <MemoryRouter>
+                <PrivateRoute
+                    isAuthenticated={ true }
+                    component={ () => <span>Listo!</span> }
+                    { ...props }
+                />
+            </MemoryRouter>
+        );
+
+        expect( wrapper.find( 'span' ).exists() ).toBe( true );
+
+    } );
+
+} );
